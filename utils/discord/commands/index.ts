0ac54a4980fe9e3694da9227d437099625e4c033
Liry24 <liry241999@gmail.com>
2025-10-01 16:34:57 +0900

    feat: add set status command for updating bot's activity status

--- utils/discord/commands/index.ts
+++ utils/discord/commands/index.ts
@@ -1,9 +1,11 @@
 import { helloCommand } from './hello'
 import { issueApiKeyCommand } from './issueApiKey'
 import { requestAccessCommand } from './requestAccess'
+import { setStatusCommand } from './setStatus'
 
 export const discordCommands: DiscordCommand[] = [
     helloCommand,
     issueApiKeyCommand,
     requestAccessCommand,
+    setStatusCommand,
 ]
